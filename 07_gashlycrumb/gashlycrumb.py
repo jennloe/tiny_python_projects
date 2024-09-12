e3ce7d85644d7a6602de9368cc705db876349db1
Jennifer Loe <jloe@sandia.gov>
2024-09-11 18:10:05 -0600

    This is functional.

--- 07_gashlycrumb/gashlycrumb.py
+++ 07_gashlycrumb/gashlycrumb.py
@@ -6,32 +6,48 @@ Purpose: To recite bad poetry
 """
 import argparse
 
+
 def main():
     """
     Here is main.
     """
 
     args = get_args()
-    file = args.file[0]
     letters = [s.upper() for s in args.letters]
     abc_dict = {}
-    for line in file:
-        abc_dict[line[0].upper()] = line
-    for my_letter in letters: 
+    for line in args.file:
+        abc_dict[line[0].upper()] = line.rstrip()
+    for my_letter in letters:
         line = abc_dict.get(my_letter)
-        if line: 
-            print(line.rstrip())
+        if line:
+            print(line)
         else:
             print(f'I do not know "{my_letter}".')
 
-        
+
 def get_args():
-    parser = argparse.ArgumentParser(description="Give a file and letters from which to extract lines.",
-                                     formatter_class= argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('letters', nargs='+', help= "One or more letters to look up.")
-    parser.add_argument('--file', type=argparse.FileType('rt'), nargs=1, default=['gashlycrumb.txt'],
-                        help= "Name of file to reference. Each line must start with a unique letter.")
+    """
+    Parse and return arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="Give a file and letters from which to extract lines.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("letters", nargs="+", help="One or more letters to look up.")
+    parser.add_argument(
+        "--file",
+        "-f",
+        type=argparse.FileType("rt"),
+        # nargs=1, # DON'T put this because it makes you return a list.
+        # argparse will always error with more than one
+        # input if you didn't tell it to accept more than one.
+        # So you don't need to specify this.
+        default="gashlycrumb.txt",
+        help="Name of file to reference. \
+                              Each line must start with a unique letter.",
+    )
     return parser.parse_args()
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     main()
