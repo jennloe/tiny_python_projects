a1503ccf5e1e66f12f03f00cec2329fbec3eb935
Jennifer Loe <jloe@sandia.gov>
2024-09-13 18:42:38 -0600

    First version that passes all tests.

---
+++ 08_apples_and_bananas/apples.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python3
+'''
+Author: Jennifer Loe
+Date: 9/13/24
+Purpose: Make up silly apple songs.
+'''
+import argparse
+import os
+
+
+def main():
+    """
+    User gives a vowel; we make it a song.
+    """
+    args = get_args()
+
+    v = args.vowel
+    text = args.text
+    if os.path.isfile(text):
+        fh = open(text)
+        text = fh.read()
+        fh.close 
+
+    for x in text:
+        if x in 'aeiou':
+            print(v.lower(), end='')
+        elif x in 'AEIOU':
+            print(v.upper(), end='')
+        else:
+            print(x,end='')
+    print()
+
+def get_args():
+    parser = argparse.ArgumentParser(description= "Give a vowel and text to make a silly song.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("text",
+                        type = str,
+                        help="Text or name of file.")
+    parser.add_argument("-v","--vowel",
+                        type=str,
+                        help="The vowel to substitute.",
+                        default='a',
+                        choices=['a','e','i','o','u'])
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
