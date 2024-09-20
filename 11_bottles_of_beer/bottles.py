49f7163bc4b872b01ee142f6e89027189ca7c16a
Jennifer Loe <jloe@sandia.gov>
2024-09-20 16:43:18 -0600

    start of 11.

---
+++ 11_bottles_of_beer/bottles.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+"""
+Author: Jennifer Loe
+Date: 9/20/24
+Purpose: Sing the annoying bottles song.
+"""
+
+
+
+def main():
+    print("bottles of beer")
+
+
+if __name__ == "__main__":
+    main()
