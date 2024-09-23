8a57caae5e238bf32b03c3ee97a335f8ffc717e0
Jennifer Loe <jloe@sandia.gov>
2024-09-23 17:44:19 -0600

    11 first passing code.

--- 11_bottles_of_beer/bottles.py
+++ 11_bottles_of_beer/bottles.py
@@ -6,10 +6,35 @@ Date: 9/20/24
 Purpose: Sing the annoying bottles song.
 """
 
-
+import argparse
 
 def main():
-    print("bottles of beer")
+    args = get_args()
+
+    for n in range(args.num,0,-1):
+        end = (f"{n-1} bottle{'s' if n - 1 > 1 else ''} of beer on the wall!\n" 
+                if n-1 > 0 else f"No more bottles of beer on the wall!")
+        print(
+            f"{n} bottle{'s' if n > 1 else ''} of beer on the wall,\n"
+            f"{n} bottle{'s' if n > 1 else ''} of beer,\n"
+            f"Take one down, pass it around,\n"
+            +end)
+
+
+def get_args():
+    """ Get Arguments """
+    parser = argparse.ArgumentParser(description="Sing bottles on the wall.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-n',
+                        '--num',
+                        type=int,
+                        metavar="bottles",
+                        help="Number of bottles to start song with.",
+                        default=10)
+    args = parser.parse_args()
+    if args.num < 1:
+        parser.error(f'--num "{args.num}" must be greater than 0')
+    return args
 
 
 if __name__ == "__main__":
