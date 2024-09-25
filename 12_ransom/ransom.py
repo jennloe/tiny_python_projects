7a5a469dee047601008a96c1358952812700e3c8
Jennifer Loe <jloe@sandia.gov>
2024-09-24 20:30:56 -0600

    Outline code of 12.

---
+++ 12_ransom/ransom.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+
+"""
+Author: Jennifer Loe
+Date: 9/24/24
+Purpose: Write a ransom note from the catnapper. :P
+"""
+
+import argparse
+import os
+import random
+
+
+def main():
+    """ Print the ransom note here. """
+    args = get_args()
+    random.seed(args.seed)
+    print(args.text)
+
+
+def get_args():
+    """ Parse input arguments. """
+    parser = argparse.ArgumentParser(description="Randomly capitalize letters of text.",
+                                     formatter_class= argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('text',
+                        help='Text or file to be randomly capitalized.',
+                        type = str,
+                        metavar='text')
+    parser.add_argument('-s',
+                        '--seed',
+                        type=int,
+                        help="Seed for random generator.",
+                        default=None)
+    args = parser.parse_args()
+    if os.path.isfile(args.text):
+        with open(args.text) as fh:
+            args.text = fh.read()
+    return args
+
+
+if __name__ == "__main__":
+    main()
